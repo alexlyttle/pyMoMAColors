35ea52ce1af25f0b6874ceaae01c950f97b6f820
Alex Lyttle <a.j.lyttle@outlook.com>
2023-10-17 11:11:22 +0100

    Rename utils to plotting

--- src/momacolors/plotting.py
+++ src/momacolors/plotting.py
@@ -1,16 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_cmaps(cmap_category, cmap_list):
-    # Create figure and adjust figure height to number of colormaps
-    nrows = len(cmap_list)
+def plot_cmaps(cmaps: list):
+    """Plot colormaps in list `cmaps`"""
+    nrows = len(cmaps)
     figh = 0.35 + 0.15 + (nrows + (nrows-1)*0.1)*0.22
     fig, axs = plt.subplots(nrows=nrows, figsize=(6.4, figh))
     fig.subplots_adjust(top=1-.35/figh, bottom=.15/figh, left=0.2, right=0.99)
 
-    axs[0].set_title(f"{cmap_category} colormaps", fontsize=14)
-
-    for ax, cmap in zip(axs, cmap_list):
+    for ax, cmap in zip(axs, cmaps):
         x = np.linspace(0, 1, cmap.N)[None, :]
         ax.imshow(x, aspect='auto', cmap=cmap)
         ax.text(-.01, .5, cmap.name, va='center', ha='right', fontsize=10,
