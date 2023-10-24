b72e401d855647bc4f7431844f9e04ce4f07d711
Alexander Lyttle <a.j.lyttle@outlook.com>
2023-10-24 22:18:12 +0100

    Make plotting emulate original package

--- src/momacolors/plotting.py
+++ src/momacolors/plotting.py
@@ -1,25 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from typing import Optional
+from math import ceil
+from .palettes import Palette
 
 def plot_cmap(cmap: ListedColormap, ax=None):
     if ax is None:
-        _, ax = plt.subplots(figsize=(6.4, 0.72))
+        _, ax = plt.subplots(figsize=(3, 3), tight_layout=True)
     x = np.linspace(0, 1, cmap.N)[None, :]
     ax.imshow(x, aspect='auto', cmap=cmap)
     ax.set_axis_off()
     return ax
 
-def plot_cmaps(cmaps: list):
+def plot_cmaps(cmaps: list, ncols: int=6, figwidth: int=8):
     """Plot colormaps in list `cmaps`"""
-    nrows = len(cmaps)
-    figh = 0.35 + 0.15 + (nrows + (nrows-1)*0.1)*0.22
-    fig, axs = plt.subplots(nrows=nrows, figsize=(6.4, figh))
-    fig.subplots_adjust(top=1-.35/figh, bottom=.15/figh, left=0.2, right=0.99)
-
+    nrows = ceil(len(cmaps) / ncols)
+    figsize = (
+        figwidth, figwidth * nrows/ncols
+    )
+    fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
+    axs = axes.ravel()
     for ax, cmap in zip(axs, cmaps):
         ax = plot_cmap(cmap, ax=ax)
-        ax.text(-.01, .5, cmap.name, va='center', ha='right', fontsize=10,
-                transform=ax.transAxes)
+        # ax.set_box_aspect(0.75)
+        ax.set_title(cmap.name, fontsize=10)
 
-    return fig, axs
+    remaining = len(cmaps) - nrows*ncols
+    if remaining < 0:
+        for ax in axs[remaining:]:
+            ax.remove()
+
+    fig.tight_layout()
+    return fig, axes
